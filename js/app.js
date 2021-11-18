d5b31340d09b6c82fe5f53395a8aca6951fc43ad
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2021-11-18 10:20:59 +0800

    book modif

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 import { Scene } from 'three';
 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
 const book=document.querySelector(".book");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
