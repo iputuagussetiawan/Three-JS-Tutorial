6cbab418325508c5e0faf8a6e56762ac53e56bbb
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2021-11-19 09:57:27 +0800

    setup scene camera object

--- js/app.js
+++ js/app.js
@@ -6,145 +6,39 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
-let scene;
-let camera;
-let renderer;
-let house;
-let model_container = document.querySelector('.webgl');
-const canvasSize = document.querySelector('.canvas-element');
 
-const stats = new Stats()
-document.body.appendChild(stats.domElement);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
+/**
+ * 1.FOV How Hide Is UR Camera
+ * 2.Aspect Ratio
+ * 3. Near Clip : Seberapa Dekat Kamera Bisa Melihat
+ * 4. Far Clip : Seberapa Jauh Kamera Bisa Melihat
+ */
 
-const init = () => {
-    // scene setup
-    scene = new THREE.Scene();
-    scene.add(new THREE.AxesHelper(5))
+ const renderer = new THREE.WebGLRenderer();
+ renderer.setSize( window.innerWidth, window.innerHeight );
+ document.body.appendChild( renderer.domElement );
 
-    //camera setup
-    const fov = 50;
-    const aspect = canvasSize.offsetWidth / canvasSize.offsetHeight;
-    const near = 0.1;
-    const far = 200;
+ const box = new THREE.BoxGeometry(1,1,1);
+ const boxMat = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+ const boxMesh = new THREE.Mesh( box, boxMat );
+ scene.add( boxMesh );
 
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 15);
-    camera.lookAt(scene.position);
-    scene.add(camera);
+ camera.position.z = 5;
 
-    //renderer setup
-    renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-        canvas: model_container
-    });
-    
-    renderer.setSize(canvasSize.offsetWidth, canvasSize.offsetHeight);
-    renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
-    renderer.autoClear = false;
-    renderer.setClearColor(0x000000, 0.0);
+ const animate = function () {
+     requestAnimationFrame( animate );
+     boxMesh.rotation.x += 0.01;
+     boxMesh.rotation.y += 0.01;
+     renderer.render( scene, camera );
+ };
 
-    //orbitcontrol setup
-    const controls = new OrbitControls(camera, renderer.domElement);
+ animate();
 
-    // ambient light setup
-    const amibientLight = new THREE.AmbientLight(0x404040, 2);
-    scene.add(amibientLight);
-
-    // direction lights setup
-    const spotLight1 = new THREE.SpotLight(0x1d27f0, 5);
-    spotLight1.position.set(6, 11, 6);
-    spotLight1.castShadow = true;
-    const spotLightHelper1 = new THREE.SpotLightHelper(spotLight1, 1, 0x00ff00);
-    scene.add(spotLight1);
-
-    // orenge light setup
-    const spotLight2 = new THREE.SpotLight(0xf57d22, 2);
-    spotLight2.position.set(-10, 0, 12);
-    spotLight2.castShadow = true;
-    const spotLightHelper2 = new THREE.SpotLightHelper(spotLight2, 2, 0x00ff00);
-    scene.add(spotLight2);
-
-    // back light setup
-    const spotLight3 = new THREE.SpotLight(0x1d27f0, 2);
-    spotLight3.position.set(-10, 18, -17);
-    spotLight3.castShadow = true;
-    const spotLightHelper3 = new THREE.SpotLightHelper(spotLight3, 2, 0xff0000);
-    scene.add(spotLight3);
-
-
-    // helper code for setting light position
-    const gui = new GUI();
-
-    // blue light controls
-    const blueLight = gui.addFolder('BlueLight');
-    blueLight.add(spotLight1.position, "x", -30, 30, 1);
-    blueLight.add(spotLight1.position, "y", -30, 30, 1);
-    blueLight.add(spotLight1.position, "z", -30, 30, 1);
-
-    // orenge light controls
-    const orengeLight = gui.addFolder('OrengeLight');
-    orengeLight.add(spotLight2.position, "x", -40, 40, 1);
-    orengeLight.add(spotLight2.position, "y", -40, 40, 1);
-    orengeLight.add(spotLight2.position, "z", -40, 40, 1);
-
-    // back light controls
-    const backLight = gui.addFolder('BackLight');
-    backLight.add(spotLight3.position, "x", -40, 40, 1);
-    backLight.add(spotLight3.position, "y", -40, 40, 1);
-    backLight.add(spotLight3.position, "z", -40, 40, 1);
-
-    // loding gltf 3d model
-    const loader = new GLTFLoader();
-    loader.load('./assets/gltf/bottle/scene.glb', (gltf) => {
-        house = gltf.scene.children[0];
-        house.scale.set(0.3, 0.3, 0.3)
-        //house.position.set(0, -1.3, 0)
-        house.position.set(-6.5, -7, 0)
-        //house.rotation.x = Math.PI / -3
-        scene.add(gltf.scene);
-    });
-
-    animate();
-}
-
-
-
-const render = () => {
-    renderer.render(scene, camera);
-}
-
-// animation recursive function
-let step = 0
-let currentTimeLine=window.pageYOffset / 3000;
-let aimTimeLine=window.pageYOffset / 3000;
-const animate = () => {
-    renderer.render(scene,camera);
-    requestAnimationFrame(animate);
-    currentTimeLine +=(aimTimeLine-currentTimeLine) * 0.01;
-    const rz=(currentTimeLine * 0.9 + 0.1) * Math.PI*2;
-
-    if(house){
-       //house.rotation.z = currentTimeLine;
-        //house.rotation.x += 0.01;
-        //house.rotation.y += 0.01;
-    }
-    
-    render();
-}
-// making responsive
-const windowResize = () => {
-    camera.aspect = canvasSize.offsetWidth / canvasSize.offsetHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(canvasSize.offsetWidth, canvasSize.offsetHeight);
-    render();
-}
-
-window.addEventListener('resize', windowResize, false);
-window.addEventListener("scroll",function(){
-    aimTimeLine=window.pageYOffset / 3000;
-})
-
-window.onload = init;
-
+ window.addEventListener('resize',function(){
+     renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+     camera.aspect=this.window.innerWidth/ this.window.innerHeight;
+     camera.updateProjectionMatrix();
+ })
 
